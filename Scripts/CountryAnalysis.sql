718b4c036b3a3d2bf089e56f7e4def49edf0489d
Ann Amboka (Anna) <amukasahann@gmail.com>
2026-02-21 09:55:11 +0300

    Update CountryAnalysis.sql

--- Scripts/CountryAnalysis.sql
+++ Scripts/CountryAnalysis.sql
@@ -5,6 +5,12 @@ Country,
 CONCAT(ROUND(CAST(SUM(Clicked) AS FLOAT) / COALESCE(SUM(Impression), 0),2),'%')AS CTR,
 ROUND(CAST(SUM(Opted_in) AS FLOAT)/COALESCE(SUM(Clicked),0),2) AS LeadRate,
 ROUND(CAST(SUM(Content_Consumed) AS FLOAT) /COALESCE(SUM(Opted_in),0),2) AS EngagementRate,
-CONCAT(ROUND(CAST(SUM(Purchased) AS FLOAT) /COALESCE(SUM(Clicked),0),2), '%')AS SalesRate
+CONCAT(ROUND(CAST(SUM(Purchased) AS FLOAT) /COALESCE(SUM(Clicked),0),2), '%')AS SalesRate,
+SUM(Cost)/SUM(Clicked)AS CPC,
+SUM(Cost)/SUM(Opted_in) AS CPL,
+ROUND(CAST(SUM(Cost) AS FLOAT)/SUM(Purchased),2) AS CPA,
+ROUND(SUM(Revenue)/SUM(Clicked),2) AS EPC,
+ROUND(SUM(Revenue)/SUM(Opted_in),2) AS EPL,
+ROUND(SUM(Revenue)/SUM(Purchased) ,2) AS EPA
 FROM dbo.Funnel_Data
 GROUP BY Country
