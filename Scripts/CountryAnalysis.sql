dca7343f1da763131701fb713d75857ab8b8a2a9
Ann Amboka (Anna) <amukasahann@gmail.com>
2026-02-21 09:48:56 +0300

    Create CountryAnalysis.sql

---
+++ Scripts/CountryAnalysis.sql
@@ -0,0 +1,10 @@
+--Analysing campaigns across the Countries--
+
+SELECT
+Country,
+CONCAT(ROUND(CAST(SUM(Clicked) AS FLOAT) / COALESCE(SUM(Impression), 0),2),'%')AS CTR,
+ROUND(CAST(SUM(Opted_in) AS FLOAT)/COALESCE(SUM(Clicked),0),2) AS LeadRate,
+ROUND(CAST(SUM(Content_Consumed) AS FLOAT) /COALESCE(SUM(Opted_in),0),2) AS EngagementRate,
+CONCAT(ROUND(CAST(SUM(Purchased) AS FLOAT) /COALESCE(SUM(Clicked),0),2), '%')AS SalesRate
+FROM dbo.Funnel_Data
+GROUP BY Country
